629263e267f2e915a58c1f802ec19277845aa191
Jaime JGB. Gonzalez Bravo <jgonzalez@GRUPOCFI.local>
2022-11-14 13:29:56 +0100

    iniciando el registro

--- src/Controller/PerfilController.php
+++ src/Controller/PerfilController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class PerfilController extends AbstractController
 {
@@ -14,5 +15,36 @@ class PerfilController extends AbstractController
         return $this->render('perfil/index.html.twig', [
             'controller_name' => 'PerfilController',
         ]);
+    }
+
+    #[Route('/registrarse', name: 'app_registro')]
+    public function registro(AuthenticationUtils $authenticationUtils): Response
+    {
+        return $this->render('perfil/registro.html.twig', [
+            'error' => $authenticationUtils->getLastAuthenticationError(),
+            'last_username' => $authenticationUtils->getLastUsername(),
+        ]);
+
+    }
+
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
