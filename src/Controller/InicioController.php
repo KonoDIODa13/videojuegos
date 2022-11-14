085f2e70042577fcb2367e324003978eb54a4d6e
Jaime JGB. Gonzalez Bravo <jgonzalez@GRUPOCFI.local>
2022-11-14 11:54:58 +0100

    controllers

--- src/Controller/InicioController.php
+++ src/Controller/InicioController.php
@@ -6,15 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LoginController extends AbstractController
+class InicioController extends AbstractController
 {
-    #[Route('/login', name: 'app_login')]
+    #[Route('/', name: 'app_inicio')]
     public function index(): Response
     {
-        return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
+        return $this->render('inicio/index.html.twig', [
+            'controller_name' => 'InicioController',
         ]);
     }
-
-
 }
