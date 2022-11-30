f3f46585785d249afb78e92f1034b2b2bee7d821
Jaime JGB. Gonzalez Bravo <jgonzalez@GRUPOCFI.local>
2022-11-30 09:43:11 +0100

    ya se pueden añadir los comentarios

--- src/Controller/PerfilController.php
+++ src/Controller/PerfilController.php
@@ -7,9 +7,11 @@ use App\Entity\Videojuego;
 use App\Repository\ListaJuegosRepository;
 use App\Repository\UsuarioRepository;
 use App\Repository\VideojuegoRepository;
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class PerfilController extends ControladorBase
 {
@@ -25,27 +27,16 @@ class PerfilController extends ControladorBase
     }
 
     #[Route('/perfil', name: 'app_perfil')]
-    public function perfil(VideojuegoRepository $videojuegoRepository, ListaJuegosRepository $listaJuegosRepository): Response
+    public function perfil(ListaJuegosRepository $listaJuegosRepository): Response
     {
         $usuario = $this->getUser();
 
         $listado = $listaJuegosRepository->findBy(['usuario' => $usuario->getId()]);
-        $arrJuegos = array();
-
-        for ($i = 0; $i < count($listado); $i++) {
-            $lista = $listado[$i];
-            if ($lista != null) {
-                $juego = $videojuegoRepository->findOneBy(['id' => $lista->getVideojuego()]);
-
-                array_push($arrJuegos, $juego);
-            }
-        }
-
 
         return $this->render('perfil/perfil.html.twig', [
             'usuario' => $usuario,
             'nombre' => $usuario->getUsername(),
-            'arrjuegos' => $arrJuegos,
+            'listado' => $listado,
         ]);
     }
     #[Route('perfil/remover_juego/{videojuego}', name: 'app_remover_juego_lista')]
@@ -54,6 +45,21 @@ class PerfilController extends ControladorBase
         $usuario = $this->getUser();
         $lista = $listaJuegosRepository->findOneBy(["usuario" => $usuario->getId(), "videojuego" => $videojuego]);
         $entityManager->remove($lista);
+        $entityManager->flush();
+        return $this->redirectToRoute('app_perfil');
+    }
+    #[Route('/perfil/addComentario', name: 'app_comentario')]
+    public function addComentario(Request $request, ListaJuegosRepository $listaJuegosRepository, VideojuegoRepository $videojuegoRepository, EntityManagerInterface $entityManager): Response
+    {
+        $comentario = $request->get('comentario');
+        //dd($comentario);
+        $idVideojuego = $request->get('videojuego'); //id videojuego
+        $videojuego = $videojuegoRepository->findOneBy(['id' => $idVideojuego]);
+        //dd($videojuego);
+        $usuario = $this->getUser();
+        $lista = $listaJuegosRepository->findOneBy(["usuario" => $usuario->getId(), 'videojuego' => $videojuego]);
+        //dd($lista);
+        $lista->setComentario($comentario);
         $entityManager->flush();
         return $this->redirectToRoute('app_perfil');
     }
