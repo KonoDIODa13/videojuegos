8cb55c780af3dfb361dcd158be167ea49fec3075
KonoDIODa13 <jaimegonbra1789@gmail.com>
2022-12-22 13:37:04 +0100

    juegov2 terminado y funcional

--- src/Controller/BootstrapThemeController.php
+++ src/Controller/BootstrapThemeController.php
@@ -2,17 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\ListaJuegos;
-use App\Entity\Videojuego;
 use App\Repository\ListaJuegosRepository;
-use App\Repository\UsuarioRepository;
 use App\Repository\VideojuegoRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use App\Form\ComentarioFormType;
 
 class BootstrapThemeController extends ControladorBase
 {
@@ -20,5 +13,51 @@ class BootstrapThemeController extends ControladorBase
     public function mostrarBootstrap(): Response
     {
         return $this->render('bootstrap/inicio.html.twig');
-    }   
+    }
+    #[Route('/bootstrap/{slug}', name: 'app_juegov2')]
+    public function mostrarJuego($slug, VideojuegoRepository $videojuegoRepository, ListaJuegosRepository $listaJuegosRepository): Response
+    {
+        $usuario = $this->getUser();
+        $videojuego = $videojuegoRepository->findOneBy(['slug' => $slug]);
+        $mensaje = null;
+        $comentarios = array();
+
+        if ($usuario != null) {
+            $lista = $listaJuegosRepository->findBy(['usuario' => $usuario]);
+            foreach ($lista as $juego) {
+                if ($juego->getVideojuego() == $videojuego) {
+                    $mensaje = "Este videojuego ya esta en la lista";
+                }
+            }
+            $listado = $listaJuegosRepository->findBy(['videojuego' => $videojuego]);
+            $UserX = $listaJuegosRepository->findOneBy(['usuario' => $usuario, 'videojuego' => $videojuego]);
+            foreach ($listado as $lista) {
+                if ($UserX == null) {
+                    $comentario = "Aun no hay comentario";
+                } else {
+                    $comentario = $UserX->getComentario();
+                }
+                if ($lista->getComentario() != $comentario) {
+                    $array = (["usuario" => $lista->getUsuario(), "comentario" => $lista->getComentario()]);
+                    array_push($comentarios, $array);
+                }
+            }
+        }
+
+        $directores = $videojuego->getDirector()->toArray();
+        $generos = $videojuego->getGenero()->toArray();
+        $plataformas = $videojuego->getPlataforma()->toArray();
+        $desarrolladoras = $videojuego->getEmpresaDesarrolladora()->toArray();
+
+        return $this->render('bootstrap/juegov2.html.twig', [
+            'videojuego' => $videojuego,
+            'directores' => $directores,
+            'generos' => $generos,
+            'plataformas' => $plataformas,
+            'desarrolladoras' => $desarrolladoras,
+            'usuario' => $usuario,
+            'comentarios' => $comentarios,
+            'mensaje' => $mensaje,
+        ]);
+    }
 }
