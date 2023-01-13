d68d8b307dfc12fe25da9fec481c8837944b6163
KonoDIODa13 <jaimegonbra1789@gmail.com>
2023-01-13 09:29:27 +0100

    el prop funciona, el state no (react)

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 
 export default class RepLogApp extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            highlightedRowId: null
+        };
+    }
+
     render() {
+        const { highlightedRowId } = this.state;
+        const { withDino } = this.props;
         let dino = '';
-        if (this.props.withDino) {
+        if (withDino) {
             dino = <span>🦖</span>;
         }
 
         const repLogs = [
             { id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5 },
             { id: 2, reps: 10, itemLabel: 'Big Dio Monkey', totalWeightLifted: 180 },
-            { id: 8, reps: 4, itemLabel: 'Bleach', totalWeightLifted: 72 }
+            { id: 8, reps: 4, itemLabel: 'Uwu', totalWeightLifted: 72 }
 
         ];
         // se puede meter directamente en el dom pero esto también funciona añadiendo en el <tbody> {repLogElements}</tbody>
@@ -26,7 +37,8 @@ export default class RepLogApp extends Component {
         });*/
 
         return (
-            <div className='col-md-7 js-rep-log-table'>
+            // <React.StrictMode>
+            <div className='col-md-7'>
                 <h2>
                     {dino} Like a dino! {dino}
                 </h2>
@@ -41,7 +53,7 @@ export default class RepLogApp extends Component {
                     </thead>
                     <tbody>
                         {repLogs.map((repLog) => (
-                            <tr key={repLog.id}>
+                            <tr key={repLog.id} className={highlightedRowId === repLog.id ? 'info' : ''}>
                                 <td>{repLog.itemLabel}</td>
                                 <td>{repLog.reps}</td>
                                 <td>{repLog.totalWeightLifted}</td>
@@ -58,8 +70,41 @@ export default class RepLogApp extends Component {
                         </tr>
                     </tfoot>
                 </table>
+                <form className='form-inline'>
+                    <div className='form-group'>
+                        <label className='sr-only control-label required'
+                            htmlFor="rep_log_item">
+                            What did you lift?
+                        </label>
+                        <select id="rep_log_item"
+                            name="item"
+                            required="required"
+                            className="form-control">
+                            <option value="" defaultValue={""}>What did you lift?</option>
+                            <option value="Cat">Cat</option>
+                            <option value="fat_Dio">Big Dio Monkey</option>
+                            <option value="laptop">My Laptop</option>
+                            <option value="coffee_cup">Coffee Cup</option>
+                        </select>
+                    </div>
+                    <br />
+                    <div className="form-group">
+                        <label className="sr-only control-label required"
+                            htmlFor="rep_log_reps">
+                            How many times?
+                        </label>
+                        <input type="number" id="rep_log_reps"
+                            name="reps" required="required"
+                            placeholder="How many times?"
+                            className="form-control" />
+                    </div>
+                    <br />
+                    <button type="submit" className="btn btn-primary">I Lifted
+                        it!
+                    </button>
+                </form>
             </div>
-
+            // </React.StrictMode>
         );
     }
 }
