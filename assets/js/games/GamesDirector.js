41c27edc9157f5e4d8830e7788b8200f588f025a
KonoDIODa13 <jaimegonbra1789@gmail.com>
2023-01-23 11:51:16 +0100

    empezando directores

--- assets/js/games/GamesDirector.js
+++ assets/js/games/GamesDirector.js
@@ -6,14 +6,40 @@ export default class GamesDirector extends Component {
         super(props);
     }
 
+    verDirector(event, director) {
+        event.preventDefault();
+        window.location.href = "https://127.0.0.1:8000/admin/director/" + director;
+    }
+
+    overDirectores(event) {
+        let ª = event.target;
+        ª.style.color = "blue";
+        ª.style.cursor = "pointer";
+    }
+
+    outDirectores(event) {
+        let ª = event.target;
+        ª.style.color = "black";
+    }
+
     render() {
-        const director = this.props.game.directores;
-        console.log(director);
-
+        const directores = this.props.game.directores;
         return (
-            <div></div>
+            <div className='container'>
+                <h4>Directores:</h4>
+                <ul>
+                    {directores.map((director) => (
+                        <li
+                            key={director.toString()}
+                            onClick={(event) => this.verDirector(event, director)}
+                            onMouseOver={(event) => this.overDirectores(event)}
+                            onMouseOut={(event) => this.outDirectores(event)}>
+                            {director}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         );
-
     }
 }
 
